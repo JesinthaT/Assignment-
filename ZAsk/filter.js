595e5c61324c0318f0e22c2f4f84c445beefe88d
JesinthaT <85641666+JesinthaT@users.noreply.github.com>
2022-02-15 12:13:05 +0530

    add zask folder
    add zask folder

---
+++ ZAsk/filter.js
@@ -0,0 +1,84 @@
+function display(){
+    var x=document.querySelector(".bottom1");
+    x.style.display="block";
+}
+function remove(){
+    var x=document.querySelector(".bottom1");
+    x.style.display="none";
+}
+function display1(){
+  
+    var y=document.querySelector(".bottom2");
+    y.style.display="block";
+   
+}
+function remove1(){
+    var a=document.querySelector(".bottom2");
+    a.style.display="none";
+}
+function display2(){
+    var b=document.querySelector(".bottom3");
+    b.style.display="block";
+}
+function remove2(){
+    var b=document.querySelector(".bottom3");
+    b.style.display="none";
+}
+function dropview(){
+    var c=document.querySelector(".drop2");
+    c.style.display="block";
+}
+function removedrop(){
+    var c=document.querySelector(".drop2");
+    c.style.display="none";
+}
+function display3(){
+    var d=document.querySelector(".bottom4");
+    d.style.display="block";
+}
+function remove3(){
+    var d=document.querySelector(".bottom4");
+    d.style.display="none";
+}
+function display4(){
+    var d=document.querySelector(".bottom5");
+    d.style.display="block";
+}
+function remove4(){
+    var d=document.querySelector(".bottom5");
+    d.style.display="none";
+}
+function display5(){
+    var d=document.querySelector(".bottom6");
+    d.style.display="block";
+}
+function remove5(){
+    var d=document.querySelector(".bottom6");
+    d.style.display="none";
+}
+function display6(){
+    var d=document.querySelector(".bottom7");
+    d.style.display="block";
+}
+function remove6(){
+    var d=document.querySelector(".bottom7");
+    d.style.display="none";
+}
+function display7(){
+    var d=document.querySelector(".bottom8");
+    d.style.display="block";
+}
+function remove7(){
+    var d=document.querySelector(".bottom8");
+    d.style.display="none";
+}
+function display8(){
+    var d=document.querySelector(".bottom9");
+    d.style.display="block";
+}
+function remove8(){
+    var d=document.querySelector(".bottom9");
+    d.style.display="none";
+}
+
+
